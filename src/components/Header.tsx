67f3bbd815fbfbe12c37412af8cf3dcac74b2f1f
schrenkk <schrenkk@media-saturn.com>
2025-05-26 22:22:29 +0200

    feat(timer): hide user icon when no user

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,11 @@ import type { FC } from "react";
 import { ArrowLeft, CircleUser } from "lucide-react";
 import { useTraining } from "@/context/TrainingContext/TrainingContext";
 import { removeSearchParameters } from "@/lib/removeSearchParameters";
+import { useAuth } from "@/context/AuthContext";
 
 const Header: FC = () => {
   const { state, dispatch } = useTraining();
+  const { currentUser } = useAuth();
   const { activeView } = state;
 
   const getTitle = () => {
@@ -45,7 +47,7 @@ const Header: FC = () => {
           )}
           <h1 className="text-2xl font-bold tracking-tight">{getTitle()}</h1>
         </div>
-        {["list", "account"].includes(activeView) ? (
+        {currentUser && ["list", "account"].includes(activeView) ? (
           <CircleUser
             onClick={() => dispatch({ type: "GO_TO_ACCOUNT" })}
             color={
