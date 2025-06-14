2ff1d5b76dc9d235b721a144504548e091e362d1
schrenkk <schrenkk@media-saturn.com>
2025-06-14 13:01:16 +0200

    remove config

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,11 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
-const ReactCompilerConfig = {
-  target: '19'
-};
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
-      babel: { plugins: ["babel-plugin-react-compiler", ReactCompilerConfig] }
+      babel: { plugins: ["babel-plugin-react-compiler"] }
     }),
     tailwindcss()
   ],
