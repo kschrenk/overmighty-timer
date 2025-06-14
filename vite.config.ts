901a71be0274cb7a5df0b8b8790f7959c4c1d8e4
schrenkk <schrenkk@media-saturn.com>
2025-06-14 12:43:44 +0200

    chore(react): install compiler

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,18 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
+const ReactCompilerConfig = {
+  target: '19'
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      babel: { plugins: ["babel-plugin-react-compiler", ReactCompilerConfig] }
+    }),
+    tailwindcss()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
